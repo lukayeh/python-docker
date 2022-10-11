5e4cae607c1ad001c4bcd9a0c3e68db14bc7c84b
Luka Thomas Page <luka_page@eu-lpage-m.local>
2022-10-11 13:04:59 +0100

    Running code formatting

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,15 @@ import sys
 app = Flask(__name__)
 
 # a simple pay that says hello
-@app.route('/')
+@app.route("/")
 def hello():
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Python:</b> {version}<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), version=sys.version)
-
+    html = (
+        "<h3>Hello {name}!</h3>"
+        "<b>Hostname:</b> {hostname}<br/>"
+        "<b>Python:</b> {version}<br/>"
+    )
+    return html.format(
+        name=os.getenv("NAME", "world"),
+        hostname=socket.gethostname(),
+        version=sys.version,
+    )
