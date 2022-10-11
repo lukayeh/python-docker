5d6d9905c68be4a4cdbfeff17ec837a0b60f6428
Luka Thomas Page <luka_page@eu-lpage-m.local>
2022-10-11 09:49:25 +0100

    Various changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 import socket
+import sys
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Python:</b> {version}<br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), version=sys.version)
 
