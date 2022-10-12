60321702c40bf62248596e78f819cb25c7d84958
Luka Thomas Page <luka_page@eu-lpage-m.home>
2022-10-12 13:10:54 +0100

    Pytest for json endpoint

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,6 @@ import pytest
 
 from app import app
 
-
 @pytest.fixture
 def client():
     client = app.test_client()
@@ -14,3 +13,15 @@ def test_root(client):
 
     res = client.get("/")
     assert b"Hello world!" in res.data
+
+
+def test_json(client):
+    """Test the json route."""
+
+    res = client.get("/json")
+    assert res.status_code == 200
+    assert b"hostname" in res.data
+    assert b"mimetype" in res.data
+    assert b"name" in res.data
+    assert b"python_version" in res.data
+    assert b"user" in res.data
