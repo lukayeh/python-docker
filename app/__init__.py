110e01c3b33586338842aef2d06a4c45a0eac50b
Luka Thomas Page <luka_page@eu-lpage-m.home>
2022-10-12 12:55:17 +0100

    Formatting and new json endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask
 import socket
 import sys
+import json
+import pwd
+
+from flask import Flask, jsonify
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 csrf = CSRFProtect()
-csrf.init_app(app) # Compliant
+csrf.init_app(app)  # Compliant
+
 
 # a simple pay that says hello
 @app.route("/")
@@ -21,5 +25,18 @@ def hello():
         name=os.getenv("NAME", "world"),
         hostname=socket.gethostname(),
         version=sys.version,
-        user=os.getenv("USER", "placeholder"),
+        user=pwd.getpwuid(os.getuid())[0],
     )
+
+
+@app.route("/json")
+def json():
+    data = jsonify(
+        name=os.getenv("NAME", "world"),
+        hostname=socket.gethostname(),
+        python_version=sys.version,
+        user=pwd.getpwuid(os.getuid())[0],
+        status=200,
+        mimetype="application/json",
+    )
+    return data
