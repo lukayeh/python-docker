9def75b8e375172f2ee84681e9ef07c86e81146b
Luka Thomas Page <luka_page@eu-lpage-m.local>
2022-10-13 12:58:04 +0100

    Adding version number and updating build job to pull it in

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,16 @@ import pwd
 
 from flask import Flask, jsonify
 from flask_wtf.csrf import CSRFProtect
+from os.path import dirname
 
 app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)  # Compliant
 
+with open(dirname(__file__) + '/pkg_info.json') as fp:
+    info = json.load(fp)
+
+__version__ = info['version']
 
 # a simple pay that says hello
 @app.route("/")
@@ -18,16 +23,17 @@ def hello():
     html = (
         "<h3>Hello {name}!</h3>"
         "<b>Hostname:</b> {hostname}<br/>"
-        "<b>Python:</b> {version}<br/>"
+        "<b>Python:</b> {pyversion}<br/>"
         "<b>User:</b> {user}<br/>"
+        "<b>Version:</b> {version}<br/>"
     )
     return html.format(
         name=os.getenv("NAME", "world"),
         hostname=socket.gethostname(),
-        version=sys.version,
+        pyversion=sys.version,
+        version=__version__,
         user=pwd.getpwuid(os.getuid())[0],
     )
-
 
 @app.route("/json")
 def json():
@@ -36,6 +42,7 @@ def json():
         hostname=socket.gethostname(),
         python_version=sys.version,
         user=pwd.getpwuid(os.getuid())[0],
+        version=__version__,
         status=200,
         mimetype="application/json",
     )
